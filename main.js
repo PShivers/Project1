ee5e563de729adb140f198814454f8101460df65
Paul Shivers <paulsshivers@gmail.com>
2019-04-10 19:46:11 -0400

    established for loop to get unique id

--- main.js
+++ main.js
@@ -1,13 +1,32 @@
+let qAndA = [
+    {
+        id: "c1q1",
+        question: "what?",
+        ans1: "chicken butt"
+    },
+    {
+        id: "c1q2",
+        question: "why?",
+        ans1: "chicken thigh"
+    }
+]
+
+
 $(function () {
-    console.log("js linked");
-
-    //event listener that on click sends element to variable and then sends variable to function to display modal
-
+    console.log("js + jquery linked");
     //clicking any question brings up modal
     $(".question").click(function () {
         //store value of div clicked in variable
-        let currentQuestion = $(this).attr('class').split(' ')[1];
-        console.log(currentQuestion);
+        let selectedQ = $(this).attr('class').split(' ')[1]
+        console.log(selectedQ);
+        console.log(qAndA[0]);
+        for (let i = 0; i < qAndA.length; i++) {
+            if (selectedQ === qAndA[i].id) {
+                console.log("matching")
+                //then push items inside question[i] to modal
+                // $(".currentQText").html()
+            }
+        }
         // let currentQuestion =
         $(".modalOverlay").show();
     });
