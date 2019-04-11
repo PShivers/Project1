f6bb6cf36c14bd5e24323d52f2d11024626fbdf1
Paul Shivers <paulsshivers@gmail.com>
2019-04-10 20:23:13 -0400

    added ans button functionality, need to determine if correct

--- main.js
+++ main.js
@@ -4,30 +4,44 @@ let qAndA = [
         question: "chicken butt",
         ans1: "what is guess what?",
         ans2: "what is guess why?",
-        ans3: "what is guess how?"
+        ans3: "what is guess how?",
+        rightAns: "ans1",
+        amount: 400
     },
     {
         id: "c1q2",
         question: "why?",
-        ans1: "chicken thigh"
+        ans1: "chicken thigh",
+        ans2: "",
+        ans3: "",
+        value: 400
     },
     {
         id: "c1q3",
         question: "?",
         ans1: "ifk",
         ans2: "sjf",
-        ans3: "sdsfs"
+        ans3: "sdsfs",
+        value: 600
     }
 ]
+
+let player1Score = ("$" + 0);
+let player2Score = ("$" + 0);
+
 
 
 $(function () {
     console.log("js + jquery linked");
+    $(".p1Score").html(player1Score);
+    $(".p2Score").html(player2Score);
+
     //clicking any question brings up modal
     $(".question").click(function () {
         //store value of div clicked in variable
         let selectedQ = $(this).attr('class').split(' ')[1]
         console.log(selectedQ);
+        //propagate object values into modal
         for (let i = 0; i < qAndA.length; i++) {
             if (selectedQ === qAndA[i].id) {
                 console.log("matching")
@@ -38,8 +52,12 @@ $(function () {
                 $(".ans3").html(qAndA[i].ans3);
             }
         }
-        // let currentQuestion =
+        //start timer
         $(".modalOverlay").show();
+        $(".ansBtn").click(function () {
+            let selectedA = $(this).attr('class').split(' ')[1]
+            console.log(selectedA);
+        })
     });
 
     $(".ans1, .ans2, .ans3").click(function () {
